12595a27547072e7ae9e0d9816b35e99da3ebe01
durararaw <jessica104jessica104@gmail.com>
2021-04-15 00:11:28 +0800

    Init Commit

---
+++ Code/main.py
@@ -0,0 +1,30 @@
+from cmu_112_graphics import *
+from PIL import ImageTk
+import tkinter as tk
+import string, random, time
+
+def appStarted(app):
+    # display
+    app.topMargin = 80
+    app.margin = 20
+
+#################################################
+# graphics
+#################################################
+
+def drawBasicScreen(app, canvas):
+    canvas.create_rectangle(app.margin, app.topMargin,
+        app.width - app.margin, app.height - app.margin,
+        fill = 'white', outline = 'black')
+
+def redrawAll(app, canvas):
+    drawBasicScreen(app, canvas)
+    
+#################################################
+# main
+#################################################
+def main():
+    runApp(width=1280, height=681)
+
+if __name__ == '__main__':
+    main()
